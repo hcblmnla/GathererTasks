03f1049dc640249e4536027520b67d9bff634d8b
Daniil Serov <alnmlbch@gmail.com>
2025-05-28 23:47:22 +0300

    Add parallel mapping gathering

---
+++ src/mapping.java
@@ -0,0 +1,42 @@
+final int CHUNK_SIZE = 3;
+
+<T, A, R> R parallel(
+    final List<? extends T> values,
+    final ExecutorService executor,
+    final Supplier<A> identity,
+    final BiFunction<A, ? super T, A> accumulator,
+    final BinaryOperator<A> combiner,
+    final Function<A, R> finisher
+) {
+    return values.stream()
+        .gather(Gatherers.windowFixed(CHUNK_SIZE))
+        .map(group -> CompletableFuture.supplyAsync(
+            () -> group.stream().reduce(identity.get(), accumulator, combiner),
+            executor
+        ))
+        .toList()
+        .stream()
+        .map(CompletableFuture::join)
+        .gather(Gatherers.fold(identity, combiner))
+        .map(finisher)
+        .toList()
+        .getFirst();
+}
+
+/*
+Output:
+	Hello: Hello: 1, 22, 333, Hello: 4444, 55555, 666666, Hello: 7777777, !
+ */
+
+void main() {
+    System.out.println(
+        parallel(
+            List.of(1, 2, 3, 4, 5, 6, 7),
+            Executors.newVirtualThreadPerTaskExecutor(),
+            () -> "Hello: ",
+            (String s, Integer n) -> s + "%d".formatted(n).repeat(n) + ", ",
+            String::concat,
+            (String s) -> "\t%s!".formatted(s)
+        )
+    );
+}
